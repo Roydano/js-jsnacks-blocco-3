f1096a19a67107325c0fed9d7f84a918561834fb
Roydano <roy.dano90@gmail.com>
2021-06-10 15:23:05 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// *Si scriva una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).  La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
+
+// *Dare la possibilità di inserire due parole. Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.  Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+// *3.  Scrivi una funzione che fonda due array (con lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall’altro. Es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+// *4. Scrivi una funzione che prenda una stringa e la trasformi con l'iniziale maiuscola e tutto il resto in minuscolo.
+
+
+
+
+
+function between(array, min, max){
+
+    var newArray = [];
+
+    for(var i = )
+}
